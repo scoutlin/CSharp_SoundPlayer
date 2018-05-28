afadea14a76906d69cf3099a49e0adb6c652a9d0
DESKTOP-MGHFEMN\ScottLin <scoutlinn@gmail.com>
2018-05-28 11:42:25 +0800

    V1.0.0

---
+++ SoundPlayer/SoundPlayer/SoundPlayer.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace SoundPlayer {
+    public partial class SoundPlayer : Form {
+
+        SoundPlayerDll.SoundPlayerDll gSoundPlayerDll;
+
+        public SoundPlayer() {
+            InitializeComponent();           
+        }
+
+        private void Init() {
+            gSoundPlayerDll = new SoundPlayerDll.SoundPlayerDll();
+            gSoundPlayerDll.SetSoundPath(textBox_SoundPath.Text);
+        }
+
+        private void SoundPlayer_Load(object sender, EventArgs e) {
+            Init();
+        }
+
+        private void button_PlaySound_Click(object sender, EventArgs e) {
+            gSoundPlayerDll.Play();
+        }
+
+        private void button_StopSound_Click(object sender, EventArgs e) {
+            gSoundPlayerDll.Stop();
+        }
+
+        private void button_VolumeDown_Click(object sender, EventArgs e) {
+            gSoundPlayerDll.VolDown();
+        }
+
+        private void button_VolumeUp_Click(object sender, EventArgs e) {
+            gSoundPlayerDll.VolUp();
+        }
+
+        private void button_Mute_Click(object sender, EventArgs e) {
+            gSoundPlayerDll.Mute();
+        }
+    }
+}
